1ef0ff391b41ff004a42cdc4f6e07dcf538d040e
h0ru5 <horus@local.host>
2014-06-29 23:02:44 +0200

    tidied resorce object

--- index.js
+++ index.js
@@ -7,36 +7,42 @@ var WebSocketServer = require('ws').Server
 //
 // so far, its just the payload, but it should actually be a full http response
 
-//This is my ressource, it changes every second and can be polled and observed
-//TODO use an actual object, not this (whatever I meant to do here)
-var ressource = {
+//This is my ressource, it changes every second and can be observed
+var resource = {
     value : 0,
-    observers : [],
+    observers : new Array(),
+    
     change : function() {
-        ressource.value++;
-	for(var i =0; i<ressource.observers.length; i++) {
-	    ressource.observers[i](ressource.value);
-	}
+        this.value++;
+        this.observers.forEach(function(callback) {
+            callback(this.value);
+        },this);
     },
+    
     _intervalId : null,
     startChanges : function() {
-       _intervalId = setInterval(ressource.change,1000);
+       this._intervalId = setInterval(
+           function(resource) {
+               resource.change(); 
+           },1000,this);
     },
     stopChanges : function() {
-        if(_intervalId)
-	   clearInterval(_intervalId);
+        if(this._intervalId)
+	   clearInterval(this._intervalId);
     }
 };
 
-
+//this function handles GET requests
 var poller  = function(req, res) {
     res.writeHead(200);
-    res.end('polling result is ' + ressource.value);
+    res.end('polling result is ' + resource.value);
 };
 
+//our server
 var server = http.createServer(poller);
 server.listen(8088);
 
+//this is a websocketserver, which handles requests that includes the headers connection:upgrade and upgrade:websocket
 var wss = new WebSocketServer({server: server});
 wss.on( 'connection', function ( ws) {
     console.log('got connection, registering observer');
@@ -45,20 +51,23 @@ wss.on( 'connection', function ( ws) {
 	//TODO it should send another http response
         ws.send(JSON.stringify(val));
     };
-    ressource.observers.push(observer);
+    resource.observers.push(observer);
 
     //TODO this should actually be in the http response
-    ws.send(JSON.stringify(val));
+    ws.send(JSON.stringify(resource.value));
 
     ws.on('close',function() {
         //TODO there should be an id for each each observer matching its ws
         console.log('closed connection, de-registering observer');
-        ressource.observers.pop();
+        resource.observers.pop();
     });
 
+    //we could also have streaming push, sort of a reversed observe using post
+    //which is not defined in coap 
     ws.on( 'message', function ( message ) {
         console.log( message );
     });
 });
 
-ressource.startChanges();
+resource.startChanges();
+console.log('up & running on port 8088');
